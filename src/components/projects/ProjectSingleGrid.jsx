34ce11c9c8eb8f448e44718baa566997c8157836
GVS <johndoe@exemple.com>
2024-06-11 13:56:23 +0200

    Lighthouse : asscesibilité

--- src/components/projects/ProjectSingleGrid.jsx
+++ src/components/projects/ProjectSingleGrid.jsx
@@ -1,5 +1,5 @@
-import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 const ProjectSingleGrid = ({ id, title, image }) => {
   return (
@@ -7,22 +7,25 @@ const ProjectSingleGrid = ({ id, title, image }) => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, delay: 1 }}
       transition={{
-        ease: 'easeInOut',
+        ease: "easeInOut",
         duration: 0.7,
         delay: 0.15,
       }}
       className="w-full max-w-xs" // Add Tailwind classes for width
     >
       <Link to={`/projects/single-project/${id}`} aria-label="Single Project">
-        <div className="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark" style={{ width: '100%', height: '350px' }}>
-          <div style={{ height: '70%' }}>
+        <div
+          className="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark"
+          style={{ width: "100%", height: "350px" }}
+        >
+          <div style={{ height: "70%" }}>
             <img
               src={image}
               className="rounded-t-xl border-none w-full h-full object-cover"
               alt="Single Project"
             />
           </div>
-          <div className="text-center px-4 py-6" style={{ height: '30%' }}>
+          <div className="text-center px-4 py-6" style={{ height: "30%" }}>
             <p className="font-general-medium text-lg md:text-xl text-ternary-dark dark:text-ternary-light mb-2">
               {title}
             </p>
